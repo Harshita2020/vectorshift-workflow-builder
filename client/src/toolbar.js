87b7ccf8e92bacad5f24bf0b4650121c17114e0e
Harshita2020 <harshitaautocad@gmail.com>
2026-05-24 22:47:35 +0530

    feat:added 5 new nodes

--- client/src/toolbar.js
+++ client/src/toolbar.js
@@ -1,17 +1,27 @@
-// toolbar.js
-
 import { DraggableNode } from './draggableNode';
 
 export const PipelineToolbar = () => {
+  return (
+    <div style={{ padding: '10px' }}>
+      <div
+        style={{
+          marginTop: '20px',
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: '10px',
+        }}
+      >
+        <DraggableNode type='customInput' label='Input' />
+        <DraggableNode type='llm' label='LLM' />
+        <DraggableNode type='customOutput' label='Output' />
+        <DraggableNode type='text' label='Text' />
 
-    return (
-        <div style={{ padding: '10px' }}>
-            <div style={{ marginTop: '20px', display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
-                <DraggableNode type='customInput' label='Input' />
-                <DraggableNode type='llm' label='LLM' />
-                <DraggableNode type='customOutput' label='Output' />
-                <DraggableNode type='text' label='Text' />
-            </div>
-        </div>
-    );
+        <DraggableNode type='filter' label='Filter' />
+        <DraggableNode type='delay' label='Delay' />
+        <DraggableNode type='api' label='API' />
+        <DraggableNode type='transform' label='Transform' />
+        <DraggableNode type='math' label='Math' />
+      </div>
+    </div>
+  );
 };
